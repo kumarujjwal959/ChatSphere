408b0ec694cbb5c698e26dfa3011bc8483eac634
Ujjwal Kumar <Kumarujjwal959@gmail.com>
2024-06-10 22:52:38 +0530

    frontend code pushed!

---
+++ frontend/src/context/authContext.jsx
@@ -0,0 +1,40 @@
+import Cookies from "js-cookie";
+import React, { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const setAuthenticated = (value) => {
+    setIsAuthenticated(value);
+  };
+  const checkAuth = () => {
+    console.log("All cookies:", Cookies.get());
+    const token = Cookies.get("authToken");
+    console.log("Checking authentication...", token);
+    if (token) {
+      console.log("Token exists. Setting authenticated to true.");
+      setAuthenticated(true);
+      console.log(isAuthenticated);
+    } else {
+      console.log("Token does not exist. Setting authenticated to false.");
+      setAuthenticated(false);
+    }
+  };
+
+  const logout = () => {
+    Cookies.remove("authToken");
+    setAuthenticated(false);
+  };
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, setAuthenticated, checkAuth, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
